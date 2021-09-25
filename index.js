2e7fd8ac63c2dd2e18c8c6aba05269cf5d67859e
Shekhar <meet.shekhar2009@gmail.com>
2021-09-25 18:08:41 +0000

    changes by server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 let port = process.env.port || 3300;
-app.get('/', "10.2.0.4", (req, res) => {
+app.get('/',  (req, res) => {
     let id = (Math.random() * 1000000000).toFixed(0);
     let name = id + ' Welcome to sample node app!';
 
